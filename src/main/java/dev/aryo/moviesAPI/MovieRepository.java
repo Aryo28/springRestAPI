b1c7a35ef1d2929881b6ba9a9eae9ef8a58abf0f
Armando Wong <armandorw28@gmail.com>
2023-01-21 23:42:01 -0600

    movie and reviews class added

---
+++ src/main/java/dev/aryo/moviesAPI/MovieRepository.java
@@ -0,0 +1,14 @@
+package dev.aryo.moviesAPI;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
+}
